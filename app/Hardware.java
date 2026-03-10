215603af6ba1e487553f4c96ef213fe489b642e3
9203 <ftc9203@beougher.com>
2026-03-10 18:33:44 -0500

    Initial commit

---
+++ app/Hardware.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+
+public class Hardware {
+
+    public enum Motor_E
+    {
+        LeftFront,
+        RightFront,
+        LeftBack,
+        RightBack,
+    }
